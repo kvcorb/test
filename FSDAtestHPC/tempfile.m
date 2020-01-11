920e6ac8193b542024c52359fb06eb7688f3207d
Aldo Corbellini <kvcorb@tin.it>
2020-01-11 19:12:14 +0100

    tests deleted

--- FSDAtestHPC/tempfile.m
+++ FSDAtestHPC/tempfile.m
@@ -1,20 +1,6 @@
 load tempfileWS
-% First input argument of yXplot is a structure.
-load carsmall
-x1 = Weight;
-x2 = Horsepower;    % Contains NaN data
-X=[x1 x2];
-y = MPG;    % Contaminated data
-boo=~isnan(y);
-y=y(boo,:);
-X=X(boo,:);
-Model=Model(boo,:);
-[out]=LXS(y,X,'nsamp',1000);
-[out]=FSReda(y,X,out.bs);
-% field label (rownames) is added to structure out
-% In this case datatooltip will display the rowname and not the default
-% string row...
-out.label=cellstr(Model);
-yXplot(out,'datatooltip',1)
+% modelfun is the function which specifies the link between X and beta
+modelfun = @(beta,X) X*beta(1).*exp(-beta(2)*X);
+out=tBothSides(y, X,'modelfun',modelfun,'prolik',true,'beta0',bini);
 close all
 save tempfileWS
